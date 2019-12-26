cd4204d360586d413e139f160006ef0df00cae8e
Alexander Vasilyev <melsior@gmail.com>
2019-12-26 18:56:34 +0300

    change variables

--- install_test.sh
+++ install_test.sh
@@ -2,27 +2,27 @@
 # ------------------------------------------------------------------------
 # archblocks - minimal, modular, manual Arch Linux install script
 # ------------------------------------------------------------------------
-# es@ethanschoonover.com @ethanschoonover http://github.com/altercation/archblocks
+# es@ethanschoonover.com @ethanschoonover http://github.com/melsior/archblocks
 
 # INSTRUCTIONS -----------------------------------------------------------
 # boot into Arch Install media and run (for this script only):
 #
-# curl https://raw.github.com/altercation/archblocks/master/sample_installer.sh" > install.sh
+# curl https://raw.github.com/melsior/archblocks/master/sample_installer.sh" > install.sh
 #     (...manually review the code! look at the blocks in the repo, then...)
 # bash install.sh
 
 # RESPOSITORY ------------------------------------------------------------
-REMOTE=https://raw.github.com/altercation/archblocks/master
+REMOTE=https://raw.github.com/melsior/archblocks/master
 
 # CONFIG -----------------------------------------------------------------
 
-HOSTNAME=tau
-USERNAME=es
-USERSHELL=/bin/bash
+HOSTNAME=test
+USERNAME=melsior
+USERSHELL=/bin/zsh
 FONT=Lat2-Terminus16
 FONT_MAP=8859-1_to_uni
 LANGUAGE=en_US.UTF-8
-KEYMAP=us
+KEYMAP=ruwin_ctrl-UTF-8
 TIMEZONE=US/Pacific
 MODULES="dm_mod dm_crypt aes_x86_64 ext2 ext4 vfat intel_agp drm i915"
 HOOKS="base udev autodetect pata scsi sata usb usbinput consolefont encrypt filesystems fsck shutdown"
@@ -35,7 +35,7 @@ KERNEL_PARAMS="quiet i915.i915_enable_rc6=1 i915.i915_enable_fbc=1 i915.lvds_dow
 # list a url to use as a mr config file and archblocks core install will
 # su to the new user's (USERNAME above) home and bootstrap using it.
 # mr will be installed if this variable is set.
-MR_BOOTSTRAP=https://raw.github.com/altercation/es-etc/master/vcs/.mrconfig
+MR_BOOTSTRAP=https://raw.github.com/melsior/es-etc/master/vcs/.mrconfig
 
 # BLOCKS -----------------------------------------------------------------
 TIME=common/time_chrony_utc
@@ -47,8 +47,8 @@ POWER=common/power_acpi
 SENSORS=common/sensors_default
 XORG="xorg/xorg_default xorg/xorg_fonts_infinality xorg/xorg_wacom xorg/xorg_synaptics xorg/mesa_dri"
 VIDEO=video/video_intel
-DESKTOP=xorg/desktop_xmonad_minimal
-HARDWARE=hardware/laptop/lenovo_thinkpad_x220
+#DESKTOP=xorg/desktop_xmonad_minimal
+#HARDWARE=hardware/laptop/lenovo_thinkpad_x220
 APPSETS="appsets/cli_hardcore appsets/vim_basics appsets/mutt_basics appsets/git_basics appsets/server_utils appsets/chromium_basics"
 
 # EXTRA PACKAGES ---------------------------------------------------------
